899cc504e6a2d43787ee69907b6ba1c02dc70d0b
Joao <jlprcarvalho@gmail.com>
2022-07-09 11:01:59 -0300

    Create Limpeza do Windows.bat

---
+++ Limpeza do Windows.bat
@@ -0,0 +1,7 @@
+@echo off
+rem João Lucas Carvalho joao.carvalho@fatec.sp.gov.br 
+rd /s /q "C:\Windows\Temp"
+rd /s /q "C:\Windows\Prefetch"
+rd /s /q "C:\Users\JOAOAD~1\AppData\Local\Temp"
+rd /s /q "C:\Users\João\AppData\Local\Temp"
+echo "Concluido"
