663f4452d407516cf00c29d688dd88e981b93fca
João Lucas Carvalho <88744662+Joaolpridolficarvalho@users.noreply.github.com>
2023-01-29 10:59:14 -0300

    Update and rename Vírus do atalho.bat to Tool to remove shortcut virus .bat

--- Tool to remove shortcut virus .bat
+++ Tool to remove shortcut virus .bat
@@ -1,9 +1,9 @@
 @echo off
 rem João Lucas Carvalho joao.carvalho@fatec.sp.gov.br 
-set /p UNIDADE="Digite a unidade"
-CONFIMA="s"
+set /p UNIT="Enter with the unit:"
+CONFIRM="s"
 cd UNIDADE
 del autorun.inf  
 del /s /f /p *.lnk  
-CONFIMA
+CONFIRM
 attrib -R -S -H /S /D *.*
